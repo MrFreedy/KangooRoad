3f29bfaa51213e11e764ecdb471f95118bb316ae
MrFreedyTLS <paul.noguez.vlife@gmail.com>
2025-08-08 11:06:47 -0400

    fix responsive's button on Form

--- frontend/src/pages/Form/Form.tsx
+++ frontend/src/pages/Form/Form.tsx
@@ -291,7 +291,7 @@ const Form = () => {
               />
             ))}
 
-            <div className="flex flex-col-reverse sm:flex-row justify-between w-full gap-4 mt-6">
+            <div className="flex flex-col-reverse sm:flex-row justify-between w-full mb-4 sm:mb-0 gap-4 mt-4">
               <button
                 onClick={handlePrev}
                 className={`w-full sm:w-auto text-white px-4 py-2 rounded ${step === 1 ? 'bg-red-500 hover:bg-red-600' : 'bg-gray-500 hover:bg-gray-600'}`}
