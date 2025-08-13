44f71fe0e5250b9b82927cbac19a0f8a3fa1667c
MrFreedy <arthur.lenne21@gmail.com>
2025-08-13 19:56:21 -0400

    fix missing import

--- backend/routes/sections.js
+++ backend/routes/sections.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const pool = require('../db');
 
 const authenticateToken = require('../middleware/authenticateToken');
+const requireRole = require('../middleware/requireRole');
 
 router.use(authenticateToken);
 
